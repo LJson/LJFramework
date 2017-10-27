efc5902bc8fedd49fcaa62828dfba4361956d0fc
ljson <Ljson@liuweilindeMac-mini.local>
2017-10-27 10:07:49 +0800

    添加完整项目

---
+++ LJFramework/LJFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJFramework
+//
+//  Created by 刘俊超 on 2017/10/27.
+//  Copyright © 2017年 Ljson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJFramework/LJFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJFramework
+//
+//  Created by 刘俊超 on 2017/10/27.
+//  Copyright © 2017年 Ljson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
